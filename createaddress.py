614b096a6ac188b964b05c7c9506c6a3a73f1888
idlechain <163794857+idlechain@users.noreply.github.com>
2024-04-16 16:44:23 +0200

    createaddress.py

---
+++ createaddress.py
@@ -0,0 +1,37 @@
+import os
+import json
+import argparse
+from eth_keys import keys
+from eth_utils import decode_hex
+
+def generate_ethereum_wallet():
+    private_key = keys.PrivateKey(os.urandom(32))
+    public_key_hex = private_key.public_key.to_hex()
+    address = keys.PublicKey(decode_hex(public_key_hex)).to_address()
+    private_key_hex = private_key.to_hex()
+    return private_key_hex, address
+
+def save_to_json(private_key):
+    data = {"private_key": private_key}
+    with open("ltwo.json", "w") as f:
+        json.dump(data, f)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json", action="store_true", help="Generate JSON file")
+    args = parser.parse_args()
+
+    private_key, address = generate_ethereum_wallet()
+
+    if args.json:
+        if os.path.exists("ltwo.json"):
+            print("Error: El archivo 'ltwo.json' ya existe.")
+        else:
+            save_to_json(private_key)
+            print("Archivo 'ltwo.json' creado con éxito.")
+    else:
+        print("Private key:", private_key)
+        print("Public address:", address)
+
+if __name__ == "__main__":
+    main()
